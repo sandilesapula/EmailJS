a1d99b6211b423bc81d28fe4770dd594db699dc3
davidsandilesapula <davidsandilesapula@gmail.com>
2021-02-11 17:32:23 +0200

    first commit

---
+++ src/components/contactForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+import emailjs from 'emailjs-com';
+
+function ContactUs() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_24j0j5h', 'template_rcmm7ae', e.target, 'user_6SXPBOCSPpJo6Y6jRaYzH')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+    }
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={sendEmail}>
+          <div className="row pt-5 mx-auto">
+            <div className="col-8 form-group mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email Address"
+                name="email"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Subject"
+                name="subject"
+              />
+            </div>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <textarea
+                className="form-control"
+                id=""
+                cols="30"
+                rows="8"
+                placeholder="Your message"
+                name="message"
+              ></textarea>
+            </div>
+            <div className="col-8 pt-3 mx-auto">
+              <input
+                type="submit"
+                className="btn btn-info"
+                value="Send Message"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
